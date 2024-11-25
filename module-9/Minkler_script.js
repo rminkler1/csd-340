d733bf8d473ac87afc19bde7ad67f8dcc7fd5346
robertminkler <robertminkler@gmail.com>
2024-11-24 23:04:31 -0700

    Module 9.3

---
+++ module-9/Minkler_script.js
@@ -0,0 +1,8 @@
+let word1 = "Arizona";
+let word2 = "Leadership";
+let word3 = "Python";
+let word4 = "Bellevue";
+
+function changeWord(wordLoc, word) {
+    document.getElementById(wordLoc).innerHTML = word;
+}
