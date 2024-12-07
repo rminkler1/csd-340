bb3fe62b1e847e798610286882e413cbd4fce3be
robertminkler <robertminkler@gmail.com>
2024-12-07 12:51:25 -0700

    move script and css to seperate files

---
+++ module-11/scripts.js
@@ -0,0 +1,54 @@
+
+      // After page load, add a submit listener to the form that calls addElement
+      window.onload = function () {
+        document
+          .getElementById("todoForm")
+          .addEventListener("submit", addElement);
+      };
+
+      // create empty array to store the list
+      let listItems = [];
+
+      function addElement(event) {
+        // prevent browser from reloading on submit
+        event.preventDefault();
+
+        // get text Input value
+        let element = document.getElementById("textInput").value;
+
+        // add element to the array
+        listItems.push(element);
+
+        // Alert the user indicating what was added to the list and where
+        alert(
+          "Items: " +
+            element +
+            " was added to the list at position " +
+            listItems.length
+        );
+      }
+
+      function deleteElement() {
+        if (listItems.length == 0) {
+          // alert the user that the list is empty
+          alert("The list is empty. Nothing to remove.");
+        } else {
+          // remove the last item from the list and alert the user
+          let itemRemoved = listItems.pop();
+          displayElements();
+        }
+      }
+
+      // Display elements in the array
+      function displayElements() {
+        let outputString = "";
+
+        // build the output string using foreach() to loop through each array entry
+        listItems.forEach(function (item, index) {
+          outputString =
+            outputString + "Item: " + ++index + " = " + item + "<br />";
+        });
+
+        // Place the output string in the placeholder
+        document.getElementById("output").innerHTML = outputString;
+      }
